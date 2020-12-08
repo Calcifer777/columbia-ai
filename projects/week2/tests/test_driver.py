32c178d0b7cb5b56d3f20477d2371a9ad767ed4f
calcifer777 <m.filip@hotmail.it>
2020-12-08 23:59:07 +0100

    bfs, dfs project

--- projects/week2/tests/test_driver.py
+++ projects/week2/tests/test_driver.py
@@ -5,19 +5,13 @@ import pytest
 from driver import bfs_search, dfs_search
 
 
-@pytest.mark.skip()
 def test_bfs(bfs_sample):
     result = bfs_search(bfs_sample[0])
     assert result == bfs_sample[1]
 
 
-state = 1
 
 def test_dfs(dfs_sample):
-    global state
     result = dfs_search(dfs_sample[0])
-    with open(f"dfs_result_{state}.json", "w") as fp:
-        json.dump(result, fp)
-    state +=1
     assert result == dfs_sample[1]
 
